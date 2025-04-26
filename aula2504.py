f8409d2e0bf1332cf2f8c0212b38e70195e89b04
marinacar17 <marinacoutinhobgomes@gmail.com>
2025-04-25 22:24:56 -0300

    correcao

--- aula2504.py
+++ aula2504.py
@@ -1,6 +1,9 @@
 def verificar_negativo(numero):
     if numero < 0:
-        return "Esse número é negativo"
-    elif numero > 0:
-        return "Esse número é positivo"
-
+        return True
+    else:
+        return False
+    
+def contar_negativos(lista):
+    for i in lista:
+    
