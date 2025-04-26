b52836a4fb101d23ba9ed5674bc56393e3621b7c
marinacar17 <marinacoutinhobgomes@gmail.com>
2025-04-25 21:35:36 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -3,33 +3,37 @@ from operacoes import somar, subtrair, multiplicar, dividir
 def menu():
     print('Bem vindo a calculadora')
     print('Qual operação deseja executar?')
-    print('1 - Soma', '2 - Subtração', '3 - Multiplicação', '4 - Divisão', '5 - Sair', sep='\n')
+    print('1 - Soma', '2 - Subtração', '3 - Multilpicação', '4 - Divisão', '5 - Sair', sep='\n')
     oper = input('Escolha a operação: ')
-    if  oper == '5':
-        print("Operação realizada! Calculadora encerrada!")
-        return
-    else:
-        a = float(input("Digite um número: "))
-        b = float(input("Digite um segundo número: " ))
+    a = float(input("Digite um número: "))
+    b = float(input("Digite um segundo número: " ))
 
-        if oper == '1':
-            soma = somar(a,b)
-            print(soma)
+    if oper == '1':
+        somar(a,b)
+        soma = somar(a,b)
+        print(soma)
+    elif oper == '2':
+        subtrair(a, b)
+        subtracao = subtrair(a,b)
 
-        elif oper == '2':
-            subtracao = subtrair(a,b)
-            print(subtracao)
-
-        elif oper == '3':
-            multiplicacao = multiplicar(a,b)
-            print(multiplicacao)
+    elif oper == '3':
+        multiplicar(a,b)
+        multiplicacao = multiplicar(a,b)
         
-        elif oper == '4':
-            divisao = dividir(a,b)
-            print(divisao)
+    elif oper == '4':
+        dividir(a,b)
+        divisao = dividir(a,b)
+        print(divisao)
+    elif oper == '5':
+        print("Operação realizada! Calculadora encerrada!")
        
-        else:
-            print("Erro! Escolha fora das opções disponíveis")
+    else:
+        print("Erro! Escolha fora das opções disponíveis")
+        
 
 menu()
+# Regras:
+# As funções das operações devem estar em um arquivo separado chamado operacoes.py
+# O arquivo principal com código da função menu deve ser chamado calculadora.py
+# Para utilizar as infoções utiliza importação.
 
