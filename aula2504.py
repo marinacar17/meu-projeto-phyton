4074ed25f822abe30af78dfffa0c01f4e39c0e62
marinacar17 <marinacoutinhobgomes@gmail.com>
2025-05-02 17:46:09 -0300

    Add files via upload

--- aula2504.py
+++ aula2504.py
@@ -5,5 +5,11 @@ def verificar_negativo(numero):
         return False
     
 def contar_negativos(lista):
+    contador = 0 
     for i in lista:
-    
+        if verificar_negativo(i):
+            contador +=1
+    return contador
+
+a = contar_negativos([1,2,3,-4,-5,-6])
+print(a)
